4cc0880509571da7ba96196e8fa12eb13659ed51
jorgealfaro1345 <31247108+jorgealfaro1345@users.noreply.github.com>
2017-09-19 09:15:32 -0500

    Update EJERCICIO 3.cpp

--- EJERCICIO 3.cpp
+++ EJERCICIO 3.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 
 using namespace std;
-int invertir(int arr[],const int n){
-        for(int i=n;i>=0;i--){
-            cout<<arr[i];
+void invertir(int arr[],int n){
+    int j,h=n-1;
+        for(int i=0;i<n/2;i++){
+            j=arr[i];
+            arr[i]=arr[h];
+            arr[h]=j;
+            h=h-1;
         }
 }
 
 int main()
 {
     int areglo[]={1,2,3,4,5};
-    invertir(areglo,4);
-
+    invertir(areglo,5);
+    for(int i=0;i<5;i++){
+        cout<<areglo[i];
+    }
 
 }
