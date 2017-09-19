b2f75c934e764c2b6bb52f8260fb8159f0c7144f
jorgealfaro1345 <31247108+jorgealfaro1345@users.noreply.github.com>
2017-09-18 23:59:16 -0500

    Update EJERCICIO 2.cpp

--- EJERCICIO 2.cpp
+++ EJERCICIO 2.cpp
@@ -2,22 +2,19 @@
 
 using namespace std;
 
-int suma(const int arr[],const int n){
-    int res=0;
-    if(n<0){
-        return 0;
+int suma(const int a[],const int n)
+{
+    if(n==1)
+    {
+        return a[0];
+    }else{
+        return a[n-1]+suma(a,n-1);
     }
-    else{
-        res=res + arr[n];
-        suma(arr,n-1);
-        cout<<res<<":::";
-    }
-    return res;
 }
 
 int main(){
     int mat[]={2,2,3,4,5};
-    int n=4;
+    int n=5;
     cout<<"la suma es:"<<suma(mat,n);
 
 }
